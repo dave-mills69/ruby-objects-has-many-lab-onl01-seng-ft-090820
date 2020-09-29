3ccb6239455eab748ddd6154674ca0de2e80a3dd
David Mills <david_mills109@yahoo.com>
2020-09-29 00:27:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,8 @@
 class Song
-  attr_accessor :artist, :name
-
-  @@all = []
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @@all << self
-  end
-  
-  def self.all
-    @@all
   end
 
   def artist_name
